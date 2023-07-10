1f73d916de273bfda328baaaf9a710100614453c
PhamTrungHieu <phamtrunghieu230201@gmail.com>
2023-07-10 17:51:19 +0700

    commit2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>commit</code> and save to reload.
+          Edit <code>commit1</code> and save to reload.
+        </p>
+        <p>
+          Edit <code>commit2</code> and save to reload.
         </p>
         <a
           className="App-link"
