edef6831a58c6717179ea03c406988abdf29d384
PhamTrungHieu <phamtrunghieu230201@gmail.com>
2023-07-11 10:29:10 +0700

    commit3

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>commit</code> and save to reload.
+          hieupham
         </p>
         <a
           className="App-link"
